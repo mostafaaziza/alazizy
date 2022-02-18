b1e0a982cdf19e15003594fb522b947e833c4c5d
ccccdsfvc <99968028+ccccdsfvc@users.noreply.github.com>
2022-02-18 14:56:45 +0200

    Update music.py

--- program/music.py
+++ program/music.py
@@ -50,7 +50,7 @@ async def ytdl(link):
         return 0, stderr.decode()
 
 
-@Client.on_message(command(["تشغيل", f"mplay@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["شغل", f"mplay@{BOT_USERNAME}"]) & other_filters)
 async def play(c: Client, m: Message):
     replied = m.reply_to_message
     chat_id = m.chat.id
